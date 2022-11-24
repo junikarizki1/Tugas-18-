0f4cd55b9d4c802679c3044af6a4d6144f097d9f
Junika Rizki <117917208+junikarizki1@users.noreply.github.com>
2022-11-24 18:14:02 +0700

    Update soal1.cpp
    tampilan lumayan keren

--- soal1.cpp
+++ soal1.cpp
@@ -1,30 +1,34 @@
 #include <iostream>
-#include <iomanip>
+//#include <iomanip>
 using namespace std;
 
 int main() {
-    const int bpp = 2000000;
-    double jmlSks, dMahasiswa, biayaPerSks, ttlBiayaSks, ttlBiayaKuliah;
-    dMahasiswa = 200000;
-    biayaPerSks = 115000;
+    const int bpp = 2000000, dMahasiswa = 200000, biayaPerSks = 115000;
+    int jmlSks, ttlBiayaSks, ttl;
+    
+    
     
     cout <<"<!--Soal No. 1-->\n";
     cout <<"Program Menghitung Biaya Kuliah Mahasiswa\n";
-    cout <<"==========================================================\n";
     cout <<"Created By : Junika Rizki\n\n";
+    cout <<"*************************************************************\n";
     cout <<"Masukkan Jumlah Sks ";
     cin >>jmlSks;
     ttlBiayaSks = jmlSks * biayaPerSks;
-    ttlBiayaKuliah = bpp+  dMahasiswa + ttlBiayaSks;
-    cout <<setprecision(10);
-    cout <<"==========================================================\n";
-    cout <<"BPP = Rp."<<bpp<<endl;
-    cout <<"Dana Kemahasiswaan = Rp."<<dMahasiswa<<endl;
-    cout <<"Jumlah Sks Mahasiswa = "<<jmlSks <<" Sks"<<endl;
-    cout <<"Biaya Per Sks = Rp."<<biayaPerSks<<endl;
-    cout <<"Total Biaya Sks Mahasiswa = Rp."<<ttlBiayaSks <<endl;
-    cout <<"==========================================================\n";
-    cout <<"Total Biaya Kuliah Mahasiswa = Rp."<<ttlBiayaKuliah <<endl;
+    ttl = bpp+  dMahasiswa + ttlBiayaSks;
+    //cout <<setprecision(10); //Jika menggunaka float, sebaiknya aktifkan ini dan <iomanip>
+    cout <<"_____________________________________________________________\n";
+    cout <<"| Jumlah Sks Mahasiswa                    = "<<jmlSks <<" Sks"<<endl;
+    cout <<"| Biaya Per Sks                           = Rp."<<biayaPerSks <<endl;
+    cout <<"|============================================================ x \n";
+    cout <<"| Total Biaya Sks Mahasiswa               = Rp."<<ttlBiayaSks <<endl;
+    cout <<"| BPP                                     = Rp."<<bpp <<endl;
+    cout <<"| Dana Kemahasiswaan                      = Rp."<<dMahasiswa <<endl;
+    cout <<"|============================================================ + \n";
+    cout <<"|\n";
+    cout <<"| Total Biaya Kuliah Mahasiswa            = Rp."<<ttl <<endl;
+    cout <<"|                                         -------------------     \n";
+    cout <<"|____________________________________________________________";
     return 0;
 
 }
