e2519a55b00d2a593bab215d62459563a9c74660
Junika Rizki <117917208+junikarizki1@users.noreply.github.com>
2022-11-29 13:16:44 +0700

    Create tes.cpp
    tampilan good

--- tes.cpp
+++ tes.cpp
@@ -4,12 +4,11 @@ using namespace std;
 
 int main() {
     char gol;
-    int gaji = 0;
+    int gaji = 0, jamLembur = 0;
     int a = 1800000;
     int b = 2500000;
     int c = 4000000;
     int jamKerja, gajiLembur,total;
-    int jamLembur = 0;
 
     cout <<"<!--Tugas 19-->\n";
     cout <<"Program Menghitung Gaji Pegawai\n";
@@ -17,30 +16,36 @@ int main() {
     cout <<"*************************************************************\n";
     cout <<"Masukkan Golongan Pegawai ";
     cin >>gol;
-
+    
+    cout <<"_____________________________________________________________\n";
     if(gol == 'a') {
         gaji = gaji + a;
-         cout <<"Gaji Pegawai                          : Rp."<<gaji;
+         cout <<"| Gaji Pegawai Gol " <<gol <<"                        : Rp."<<gaji;
     }else if(gol == 'b') {
         gaji = gaji + b;
-         cout <<"Gaji Pegawai                          : Rp."<<gaji;
+         cout <<"| Gaji Pegawai Gol " <<gol <<"                        : Rp."<<gaji;
     }else if(gol == 'c') {
         gaji = gaji + c;
-         cout <<"Gaji Pegawai                          : Rp."<<gaji;
+         cout <<"| Gaji Pegawai Gol " <<gol <<"                        : Rp."<<gaji;
     }
-
-
-    cout <<"\n\nMasukkan Jam Kerja\n";
+    cout <<"\n| ===========================================================";
+    cout <<"\n|";
+    cout <<"\n|"<<endl;
+    cout <<"| Masukkan Jam Kerja ";
     cin >>jamKerja;
 
     if(jamKerja >150)
         jamLembur = jamKerja - 150;
     gajiLembur = jamLembur * 12000;
-    cout <<"Jam Lembur                             : "<<jamLembur <<" Jam\n";
-    cout <<"Gaji Lembur                            : Rp."<<gajiLembur;
+    cout <<"| Jam Lembur                      : "<<jamLembur <<" Jam\n";
+    cout <<"| Gaji Lembur Per Jam             : Rp.12000\n";
+    cout <<"| ===========================================================  \n";
+    cout <<"| Gaji Lembur                                : Rp."<<gajiLembur;
 
     total = gajiLembur + gaji;
-    cout <<"\nTotal Gaji                             : Rp."<<total;
+    cout <<"\n| Total Gaji                                 : Rp."<<total <<endl;
+    cout <<"|                                             ---------------\n";
+    cout <<"|____________________________________________________________";
 
 
 
