2be47c2678a13112c73a063fba05c4a5f22e5295
Junika Rizki <117917208+junikarizki1@users.noreply.github.com>
2022-12-01 11:49:12 +0700

    Create tugas 20 gaji dosen.cpp
    belum rapi

---
+++ tugas 20 gaji dosen.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int sks;
+    int gajiPsks=0;
+    int gajiKtr;
+    string gol, npwp;
+    float pajak, ttlGaji;
+
+    cout <<"Masukkan Golongan Dosen\n";
+    getline (cin, gol);
+    cout <<"Masukkan Jumlah Sks Dosen Mengajar\n";
+    cin >>sks;
+    cout <<"Apakah Dosen Memiliki NPWP ?\n";
+    cout <<"Isikan 'ada' atau 'tidak'\n\n";
+    cin >>npwp;
+
+
+    if (gol == "lektor kepala") {
+        gajiPsks = gajiPsks + 40000;
+        gajiKtr = gajiPsks * sks;
+    }else if (gol == "lektor") {
+        gajiPsks = gajiPsks + 37000;
+        gajiKtr = gajiPsks * sks;
+    }else if (gol == "asisten ahli") {
+        gajiPsks = gajiPsks + 33000;
+        gajiKtr = gajiPsks * sks;
+    } else {
+        gajiPsks = gajiPsks + 28000;
+        gajiKtr = gajiPsks * sks;
+        gol = "Tidak Ada Golongan";
+    }
+
+    if (npwp == "ada") {
+        pajak = gajiKtr * 0.025;
+    }else {
+        pajak = gajiKtr * 0.03;
+    }
+
+    ttlGaji = gajiKtr - pajak;
+    cout <<"Golongan Dosen adalah               "<<gol <<endl;
+    cout <<"Jumlah Sks Dosen                    "<<sks <<" Sks" <<endl;
+    cout <<"Keterangan NPWP                     "<<npwp <<endl;
+    cout <<"Gaji Dosen Per Sks                  Rp."<<gajiPsks <<endl;
+    cout <<"Gaji Dosen Sebelum Pajak            Rp."<<gajiKtr <<endl;
+    cout <<"Pajak                               Rp."<<pajak <<endl;
+    cout <<"Total Gaji Dosen                    Rp."<<ttlGaji;
+
+
+
+    return 0;
+}
